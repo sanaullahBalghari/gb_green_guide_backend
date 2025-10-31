a22e76c2e06a58115d73df68ff3ceae9ed971b2d
sanaullahBalghari <sanaullahakhonzada289@gmail.com>
2025-10-31 16:44:22 +0500

    update Db url

--- gb_green_guide/ecommerce/admin.py
+++ gb_green_guide/ecommerce/admin.py
@@ -22,6 +22,7 @@ class ProductCategoryAdmin(admin.ModelAdmin):
 # ===============================
 # ✅ Product Admin
 # ===============================
+# ===============================
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = (
